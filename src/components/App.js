9aa2e40f30af1d45a5f70c75a3d695fae961f574
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-02-03 14:32:22 +0100

    passed data through to Table component

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,29 @@ import Table from './Table';
 
 class App extends React.Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ];
+
     return (
       <div className="container">
-        <Table />
+        {/* Pass the data through to the child component (Table) with properties */}
+        <Table characterData={characters} />
       </div>
     );
   }
